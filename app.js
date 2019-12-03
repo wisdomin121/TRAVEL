c94b93f3f8c42edadc3f363963c48bd86facf3c0
Minji Kim <wisdomin121@naver.com>
2019-12-03 13:27:25 +0900

    change css to scss

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
