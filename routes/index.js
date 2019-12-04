8bab7b9c2284d0598a5b16b88eff163ecd435c0a
Minji Kim <wisdomin121@naver.com>
2019-12-04 14:13:45 +0900

    signin&signout complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
+  User = require('../models/user');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin');
+});
+
+router.post('/signin', function(req, res, next) {
+  User.findOne({id: req.body.id}, function(err, user) {
+    if (err) {
+      res.render('error', {message: "Error", error: err});
+    } else if (!user || user.password !== req.body.password) {  //비밀번호는 암호화 해서 넣어야 함 
+      req.flash('danger', 'Invalid username or password.');
+      res.redirect('back');
+    } else {
+      req.session.user = user;
+      res.redirect('/');
+    }
+  });
+});
+
+router.get('/signout', function(req, res, next) {
+  delete req.session.user;
+  res.redirect('/');
 });
 
 module.exports = router;
